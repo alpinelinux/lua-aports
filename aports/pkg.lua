a38f65392bc34cfd036c7af54ed4e550a061157d
Natanael Copa <ncopa@alpinelinux.org>
2014-01-09 12:44:05 +0000

    pkg: add optional subpkgname option to apk_file functions

--- aports/pkg.lua
+++ aports/pkg.lua
@@ -73,23 +73,23 @@ function M.get_repo_name(pkg)
 	return string.match(pkg.dir, ".*/(.*)/.*")
 end
 
-function M.get_apk_file_name(pkg)
-	return pkg.pkgname.."-"..pkg.pkgver.."-r"..pkg.pkgrel..".apk"
+function M.get_apk_file_name(pkg, name)
+	return (name or pkg.pkgname).."-"..pkg.pkgver.."-r"..pkg.pkgrel..".apk"
 end
 
-function M.get_apk_file_path(pkg)
+function M.get_apk_file_path(pkg, name)
 	if abuild.pkgdest ~= nil and abuild.pkgdest ~= "" then
-		return abuild.pkgdest.."/"..M.get_apk_file_name(pkg)
+		return abuild.pkgdest.."/"..M.get_apk_file_name(pkg, name)
 	end
-	if abuild.repodest ~= nil and abuild.epodest ~= "" then
-		return abuild.repodest.."/"..M.get_repo_name(pkg).."/"..abuild.arch.."/"..M.get_apk_file_name(pkg)
+	if abuild.repodest ~= nil and abuild.repodest ~= "" then
+		return abuild.repodest.."/"..M.get_repo_name(pkg).."/"..abuild.arch.."/"..M.get_apk_file_name(pkg, name)
 	end
-	return pkg.dir.."/"..M.get_apk_file_name(pkg)
+	return pkg.dir.."/"..M.get_apk_file_name(pkg, name)
 end
 
-function M.apk_file_exists(pkg)
+function M.apk_file_exists(pkg, name)
 	-- technically we check if it is readable...
-	local filepath = M.get_apk_file_path(pkg)
+	local filepath = M.get_apk_file_path(pkg, name)
 	local f = io.open(filepath)
 	if f == nil then
 		return false
