436ff64e7f3e3fbd3f6818ad627800b78bc14991
Natanael Copa <ncopa@alpinelinux.org>
2023-06-09 15:59:23 +0200

    abuild: add tests, varifous fixes
    Let envronment have precedence.
    Let functions.sh source the abuild.conf, so we respect the user config.

--- aports/abuild.lua
+++ aports/abuild.lua
@@ -1,53 +1,27 @@
-
 local M = {}
 local abuild_conf = {}
-M.conf_file = "/etc/abuild.conf"
 M.functions = "/usr/share/abuild/functions.sh"
 
 function M.get_conf(var)
 	-- check cache
-	if abuild_conf[var] ~= nil then
-		return abuild_conf[var]
-	end
-
-	-- use os env var
-	abuild_conf[var] = os.getenv(var)
-	if abuild_conf[var] ~= nil then
-		return abuild_conf[var]
-	end
-
-	-- parse config file
-	local f = io.popen(" . "..M.conf_file..' ; echo -n "$'..var..'"')
-	abuild_conf[var] = f:read("*all")
-	f:close()
-	return abuild_conf[var]
-end
-
-local function get_cached_var(var)
-	-- check cache
-	if abuild_conf[var] then
-		return abuild_conf[var]
-	end
-
-	-- use os env var
-	abuild_conf[var] = os.getenv(var)
-	if abuild_conf[var] ~= nil then
-		return abuild_conf[var]
+	local value = os.getenv(var) or abuild_conf[var]
+	if value ~= nil then
+		return value
 	end
 
 	-- execute functions.sh
-	local f = io.popen((' . %s ; echo -n "$%s"'):format(M.functions, var))
+	local f = io.popen((' . %s ; printf "%%s" "$%s"'):format(M.functions, var))
 	abuild_conf[var] = f:read("*all")
 	f:close()
 	return abuild_conf[var]
 end
 
 function M.get_arch()
-	return get_cached_var("CARCH")
+	return M.get_conf("CARCH")
 end
 
 function M.get_libc()
-	return get_cached_var("CLIBC")
+	return M.get_conf("CLIBC")
 end
 
 M.arch = M.get_arch()
@@ -55,6 +29,5 @@ M.libc = M.get_libc()
 M.repodest = M.get_conf("REPODEST")
 M.pkgdest = M.get_conf("PKGDEST")
 M.chost = M.get_conf("CHOST")
-
 
 return M
---
+++ spec/abuild_spec.lua
@@ -0,0 +1,74 @@
+-- abuild_spec.lua
+
+posix = require("posix")
+
+describe("abuild", function()
+	local tmpdir
+
+	setup(function()
+		tmpdir = os.tmpname()
+		abuild_conf = tmpdir .. "/abuild.conf"
+		os.remove(tmpdir)
+		lfs.mkdir(tmpdir)
+		local f = io.open(abuild_conf, "w")
+		f:write(
+			"CARCH=aarch64\n"
+				.. "CLIBC=musl\n"
+				.. "MYVAR=myvalue\n"
+				.. "REPODEST="
+				.. tmpdir
+				.. "\n"
+				.. "CHOST=aarch64-alpine-linux-musl\n"
+		)
+		f:close()
+		posix.stdlib.setenv("ABUILD_USERCONF", abuild_conf)
+	end)
+
+	teardown(function()
+		os.execute("rm -r " .. tmpdir)
+		posix.stdlib.setenv("ABUILD_USERCONF", nil)
+	end)
+
+	describe("get_conf", function()
+		local abuild = require("aports.abuild")
+		it("should return the value of a configuration variable from the user config", function()
+			assert.equal("myvalue", abuild.get_conf("MYVAR"))
+		end)
+	end)
+
+	describe("get_arch", function()
+		local abuild = require("aports.abuild")
+		it("should return the CARCH value from the environment", function()
+			posix.stdlib.setenv("CARCH", "foobar")
+			assert.equal("foobar", abuild.get_arch())
+			posix.stdlib.setenv("CARCH", nil)
+		end)
+
+		it("should return the CARCH value from user config", function()
+			assert.equal("aarch64", abuild.get_arch())
+			assert.equal("aarch64", abuild.arch)
+		end)
+	end)
+
+	describe("get_libc", function()
+		local abuild = require("aports.abuild")
+		it("should return the libc value from the user config", function()
+			assert.equal("musl", abuild.get_libc())
+			assert.equal("musl", abuild.libc)
+		end)
+	end)
+
+	describe("abuild.repodest", function()
+		local abuild = require("aports.abuild")
+		it("should contain the REPODEST value from the user config", function()
+			assert.equal(tmpdir, abuild.repodest)
+		end)
+	end)
+
+	describe("abuild.chost", function()
+		local abuild = require("aports.abuild")
+		it("should contain the CHOST value from the user config", function()
+			assert.equal("aarch64-alpine-linux-musl", abuild.chost)
+		end)
+	end)
+end)
